98cdcea9c5da438d8eb217414ceed299e343e2fb
Edward Z. Yang <ezyang@fb.com>
2018-10-13 23:53:47 -0400

    even better
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- gh_test.py
+++ gh_test.py
@@ -16,6 +16,9 @@ import gh
 ACCEPT = os.getenv('GH_TEST_ACCEPT')
 
 
+ACCEPT_HISTORY = {}
+
+
 def indent(text, prefix):
     """
     Poly-fill for textwrap.indent on Python 2
@@ -48,6 +51,19 @@ def escape_trailing_quote(s):
         return s
 
 
+def adjust_lineno(state, fn, lineno):
+    if fn not in state:
+        return lineno
+    for edit_loc, edit_diff in state[fn]:
+        if lineno > edit_loc:
+            lineno += edit_diff
+    return lineno
+
+
+def record_edit(state, fn, lineno, delta):
+    state.setdefault(fn, []).append(lineno, delta)
+
+
 RE_EXPECT = re.compile(r"^( *)([^\n]*?''')(.*?)(''')", re.DOTALL | re.MULTILINE)
 
 
@@ -56,29 +72,42 @@ def replace_string_literal(src, lineno, new_string):
     Replace a triple single-quoted string literal with new contents.
     Only handles printable ASCII correctly at the moment.
 
-    >>> print(replace_string_literal("'''arf'''", 1, "barf"))
-    '''barf'''
-    >>> print(replace_string_literal("  moo = '''arf'''", 1, "'a'\n\\b\n"))
+    Returns a tuple of the replaced string, as well as a delta of
+    number of lines added/removed.
+
+    >>> replace_string_literal("'''arf'''", 1, "barf")
+    ("'''barf'''", 0)
+    >>> r = replace_string_literal("  moo = '''arf'''", 1, "'a'\n\\b\n")
+    >>> print(r[0])
       moo = '''\
     'a'
     \\b
     '''
-    >>> print(replace_string_literal("    f('''\"\"\"''')", 1, "a ''' b"))
+    >>> r[1]
+    3
+    >>> replace_string_literal("  moo = '''\\\narf'''", 1, "'a'\n\\b\n")[1]
+    2
+    >>> print(replace_string_literal("    f('''\"\"\"''')", 1, "a ''' b")[0])
         f('''a \'\'\' b''')
     """
     assert all(c in string.printable for c in new_string)
     i = nth_line(src, lineno)
+    new_string = normalize_nl(new_string)
+
+    delta = [new_string.count("\n")]
+    if delta[0] > 0:
+        delta[0] += 1  # handle the extra \\\n
 
     def escape(s):
-        return escape_trailing_quote(normalize_nl(s).replace('\\', '\\\\')) \
-                .replace("'''", r"\'\'\'")
+        return escape_trailing_quote(s.replace('\\', '\\\\')).replace("'''", r"\'\'\'")
 
     def replace(m):
         inner = escape(new_string) + m.group(4)
         msg = "\\\n" + inner if "\n" in new_string else inner
+        delta[0] -= m.group(3).count("\n")
         return ''.join([m.group(1), m.group(2), msg])
 
-    return src[:i] + RE_EXPECT.sub(replace, src[i:], count=1)
+    return (src[:i] + RE_EXPECT.sub(replace, src[i:], count=1), delta[0])
 
 
 @composite
@@ -109,7 +138,7 @@ class TestFunctional(unittest.TestCase):
         r2 = '''placeholder2'''
         r3 = '''placeholder3'''
         """
-        new_prog = replace_string_literal(textwrap.dedent(prog), 2, t)
+        new_prog = replace_string_literal(textwrap.dedent(prog), 2, t)[0]
         exec(new_prog)
         msg = "program was:\n{}".format(new_prog)
         self.assertEqual(r, 'placeholder', msg=msg)  # noqa: F821
@@ -125,13 +154,25 @@ class TestExpect(unittest.TestCase):
             # current frame and parent frame, plus any requested skip
             tb = traceback.extract_stack(limit=2+skip)
             fn, lineno, _, _ = tb[0]
+            """
             with open(fn, 'r+') as f:
                 old = f.read()
-                with open(fn + ".bak", 'w') as f_bak:
-                    f_bak.write(old)
+
+                # compute the change in lineno
+                lineno = adjust_lineno(ACCEPT_HISTORY, fn, lineno)
+                delta = 
+
+                # Only write the backup file the first time we hit the
+                # file
+                if fn not in ACCEPT_HISTORY:
+                    with open(fn + ".bak", 'w') as f_bak:
+                        f_bak.write(old)
                 f.seek(0)
                 f.truncate(0)
-                f.write(old)
+                f.write(new)
+
+            record_edit(ACCEPT_HISTORY, fn, lineno, delta)
+            """
         else:
             help_text = "To accept the current output, re-run test with envvar GH_TEST_ACCEPT=1"
             if hasattr(self, "assertMultiLineEqual"):
