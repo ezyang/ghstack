fec45d3d1540d22628cc69971f51f4eafdbcca31
Edward Z. Yang <ezyang@fb.com>
2018-10-13 23:06:10 -0400

    Back up
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- gh_test.py
+++ gh_test.py
@@ -1,21 +1,27 @@
 import unittest
-import inspect
 import textwrap
 import re
 import doctest
 import sys
 import string
+import os
+import traceback
 
 from hypothesis import given, event
 from hypothesis.strategies import text, integers, composite
 
 import gh
 
+
+ACCEPT = os.getenv('GH_TEST_ACCEPT')
+
+
 def indent(text, prefix):
     """
     Poly-fill for textwrap.indent on Python 2
     """
     return ''.join(prefix+line for line in text.splitlines(True))
+
 
 def nth_line(src, lineno):
     """
@@ -30,8 +36,10 @@ def nth_line(src, lineno):
         pos = src.find('\n', pos) + 1
     return pos
 
+
 def normalize_nl(t):
     return t.replace('\r\n', '\n').replace('\r', '\n')
+
 
 def escape_trailing_quote(s):
     if s and s[-1] == "'":
@@ -39,7 +47,9 @@ def escape_trailing_quote(s):
     else:
         return s
 
+
 RE_EXPECT = re.compile(r"^( *)([^\n]*?''')(.*?)(''')", re.DOTALL | re.MULTILINE)
+
 
 def replace_string_literal(src, lineno, new_string):
     r"""
@@ -58,14 +68,18 @@ def replace_string_literal(src, lineno, new_string):
     """
     assert all(c in string.printable for c in new_string)
     i = nth_line(src, lineno)
+
     def escape(s):
         return escape_trailing_quote(normalize_nl(s).replace('\\', '\\\\')) \
                 .replace("'''", r"\'\'\'")
+
     def replace(m):
         inner = escape(new_string) + m.group(4)
         msg = "\\\n" + inner if "\n" in new_string else inner
         return ''.join([m.group(1), m.group(2), msg])
+
     return src[:i] + RE_EXPECT.sub(replace, src[i:], count=1)
+
 
 @composite
 def text_lineno(draw):
@@ -73,18 +87,20 @@ def text_lineno(draw):
     lineno = draw(integers(min_value=1, max_value=t.count("\n")+1))
     return (t, lineno)
 
+
 class TestFunctional(unittest.TestCase):
     longMessage = True
 
     @given(text_lineno())
-    def test_nth_line_ref(self, t_l):
-        t, l = t_l
-        event("lineno = {}".format(l))
+    def test_nth_line_ref(self, t_lineno):
+        t, lineno = t_lineno
+        event("lineno = {}".format(lineno))
+
         def nth_line_ref(src, lineno):
             xs = src.split("\n")[:lineno]
             xs[-1] = ''
             return len("\n".join(xs))
-        self.assertEqual(nth_line(t, l), nth_line_ref(t, l))
+        self.assertEqual(nth_line(t, lineno), nth_line_ref(t, lineno))
 
     @given(text(string.printable))
     def test_replace_string_literal_roundtrip(self, t):
@@ -96,11 +112,36 @@ class TestFunctional(unittest.TestCase):
         new_prog = replace_string_literal(textwrap.dedent(prog), 2, t)
         exec(new_prog)
         msg = "program was:\n{}".format(new_prog)
-        self.assertEqual(r, 'placeholder', msg=msg)
-        self.assertEqual(r2, normalize_nl(t), msg=msg)
-        self.assertEqual(r3, 'placeholder3', msg=msg)
+        self.assertEqual(r, 'placeholder', msg=msg)  # noqa: F821
+        self.assertEqual(r2, normalize_nl(t), msg=msg)  # noqa: F821
+        self.assertEqual(r3, 'placeholder3', msg=msg)  # noqa: F821
+
 
 class TestExpect(unittest.TestCase):
+    longMessage = True
+
+    def assertExpected(self, actual, expect, skip=0):
+        if ACCEPT:
+            # current frame and parent frame, plus any requested skip
+            tb = traceback.extract_stack(limit=2+skip)
+            fn, lineno, _, _ = tb[0]
+            with open(fn, 'r+') as f:
+                old = f.read()
+                with open(fn + ".bak", 'w') as f_bak:
+                    f_bak.write(old)
+                f.seek(0)
+                f.truncate(0)
+                f.write(old)
+        else:
+            help_text = "To accept the current output, re-run test with envvar GH_TEST_ACCEPT=1"
+            if hasattr(self, "assertMultiLineEqual"):
+                self.assertMultiLineEqual(actual, expect, msg=help_text)
+            else:
+                self.assertEqual(actual, expect, msg=help_text)
+
+    def test_sample(self):
+        self.assertExpected("foo", '''bar''')
+
     def test_replace(self):
         s = """\
         def f():
@@ -122,9 +163,11 @@ class TestGh(unittest.TestCase):
     def test_basic(self):
         pass
 
+
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(sys.modules[__name__]))
     return tests
+
 
 if __name__ == '__main__':
     unittest.main()
