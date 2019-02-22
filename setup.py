e365710fb8cf8ab07a507cacdeef87abdc6c784c
Edward Z. Yang <ezyang@fb.com>
2019-02-22 11:42:41 -0500

    0.0.3
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ghstack",
-    version="0.0.2",
+    version="0.0.3",
     author="Edward Z. Yang",
     author_email="ezyang@mit.edu",
     description="Stack diff support for GitHub",
