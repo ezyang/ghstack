d1985cc0962fef187320aed74feed182b7bc1b51
Edward Z. Yang <ezyang@fb.com>
2019-02-22 09:24:10 -0800

    Proxy support
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- ghstack/__main__.py
+++ ghstack/__main__.py
@@ -7,26 +7,46 @@ import getpass
 try:
     import configparser
 except ImportError:
-    import ConfigParser as configparser
+    import ConfigParser as configparser  # type: ignore
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Submit stack of diffs to GitHub.')
-    parser.add_argument('msg', metavar='MSG', default='Update', nargs='?', type=str, help='Description of change you made')
-    parser.add_argument('--github-v4', default=ghstack.GraphQLEndpoint('https://api.github.com/graphql'), help='GitHub GraphQL API endpoint (V4) to use')
-    parser.add_argument('--github-v3', default=ghstack.RESTEndpoint('https://api.github.com'), help='GitHub REST API endpoint (V3) to use')
+    parser = argparse.ArgumentParser(
+        description='Submit stack of diffs to GitHub.')
+    parser.add_argument(
+        'msg', metavar='MSG', default='Update', nargs='?', type=str,
+        help='Description of change you made')
+    parser.add_argument(
+        '--github-v4',
+        default=ghstack.GraphQLEndpoint('https://api.github.com/graphql'),
+        help='GitHub GraphQL API endpoint (V4) to use')
+    parser.add_argument(
+        '--github-v3',
+        default=ghstack.RESTEndpoint('https://api.github.com'),
+        help='GitHub REST API endpoint (V3) to use')
     args = parser.parse_args()
 
     config = configparser.ConfigParser()
     config.read(['.ghstackrc', os.path.expanduser('~/.ghstackrc')])
 
     write_back = False
+
     if not config.has_section('ghstack'):
         config.add_section('ghstack')
+
     if not config.has_option('ghstack', 'github_oauth'):
-        github_oauth = config.set('ghstack', 'github_oauth', getpass.getpass('GitHub OAuth token (make one at https://github.com/settings/tokens -- we need public_repo permissions): ').strip())
+        github_oauth = config.set(
+            'ghstack',
+            'github_oauth',
+            getpass.getpass(
+                'GitHub OAuth token (make one at '
+                'https://github.com/settings/tokens -- '
+                'we need public_repo permissions): ').strip())
         write_back = True
     else:
         github_oauth = config.get('ghstack', 'github_oauth')
+
+    proxy = config.get('ghstack', 'proxy', fallback=None)
 
     if write_back:
         config.write(open(os.path.expanduser('~/.ghstackrc'), 'w'))
@@ -34,8 +54,14 @@ def main():
 
     args.github_v4.oauth_token = github_oauth
     args.github_v3.oauth_token = github_oauth
+    args.github_v4.proxy = proxy
+    args.github_v3.proxy = proxy
 
-    ghstack.main(msg=args.msg, github=args.github_v4, github_rest=args.github_v3)
+    ghstack.main(
+        msg=args.msg,
+        github=args.github_v4,
+        github_rest=args.github_v3)
+
 
 if __name__ == "__main__":
     main()
