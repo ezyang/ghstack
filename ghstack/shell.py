d2d13cbdc7eceaebc7ce6e0192b3233c237ea7e2
Edward Z. Yang <ezyang@fb.com>
2019-02-25 15:16:10 -0500

    Document ghstack.shell
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- ghstack/shell.py
+++ ghstack/shell.py
@@ -6,6 +6,14 @@ import sys
 
 
 def format_env(env):
+    """
+    Formats the explicitly specified environment in a human readable way.
+
+    This isn't really actually used.
+
+    Args:
+        env: The environment dictionary you wish to format
+    """
     r = []
     for k, v in env.items():
         r.append("{}={}".format(k, subprocess.list2cmdline([v])))
@@ -13,6 +21,13 @@ def format_env(env):
 
 
 def log_command(args, env=None):
+    """
+    Given a command, print it in a both machine and human readable way.
+
+    Args:
+        *args: the list of command line arguments you want to run
+        env: the dictionary of environment variable settings for the command
+    """
     cmd = subprocess.list2cmdline(args).replace("\n", "\\n")
     # if env is not None:
     #     cmd = "{} {}".format(format_env(env), cmd)
@@ -26,13 +41,49 @@ def merge_dicts(x, y):
 
 
 class Shell(object):
+    """
+    An object representing a shell (e.g., the bash prompt in your
+    terminal), maintaining a concept of current working directory, and
+    also the necessary accoutrements for testing.
+    """
+
     def __init__(self, quiet=False, cwd=None, testing=False):
-        self.cwd = cwd
+        """
+        Args:
+            cwd: Current working directory of the shell.  Pass None to
+                initialize to the current cwd of the current process.
+            quiet: If True, suppress printing out the command executed
+                by the shell.  By default, we print out commands for ease
+                of debugging.  Quiet is most useful for non-mutating
+                shell commands.
+            testing: If True, operate in testing mode.  Testing mode
+                enables features which make the outputs of commands more
+                deterministic; e.g., it sets a number of environment
+                variables for Git.
+        """
+        self.cwd = cwd if cwd else os.getcwd()
         self.quiet = quiet
         self.testing = testing
         self.testing_time = 1112911993
 
     def sh(self, *args, **kwargs):
+        """
+        Run a command specified by args, and return string representing
+        the stdout of the run command, raising an error if exit code
+        was nonzero (unless exitcode kwarg is specified; see below).
+
+        Args:
+            *args: the list of command line arguments to run
+            env: any extra environment variables to set when running the
+                command.  Environment variables set this way are ADDITIVE
+                (unlike subprocess default)
+            stderr: where to pipe stderr; by default, we pipe it straight
+                to this process's stderr
+            input: string value to pass stdin
+            exitcode: if True, return a bool rather than string, specifying
+                whether or not the process successfully returned with exit
+                code 0.  We never raise an exception when this is True.
+        """
         stdin = None
         if 'input' in kwargs:
             stdin = subprocess.PIPE
@@ -65,6 +116,13 @@ class Shell(object):
         return out.decode()
 
     def git(self, *args, **kwargs):
+        """
+        Run a git command.  The returned stdout has trailing newlines stripped.
+
+        Args:
+            *args: Arguments to git
+            **kwargs: Any valid kwargs for sh()
+        """
         env = kwargs.setdefault("env", {})
         # Some envvars to make things a little more script mode nice
         if self.testing:
@@ -94,9 +152,26 @@ class Shell(object):
             return r.rstrip("\n")
 
     def test_tick(self):
+        """
+        Increase the current time.  Useful when testing is True.
+        """
         self.testing_time += 60
 
     def open(self, fn, mode):
+        """
+        Open a file, relative to the current working directory.
+
+        Args:
+            fn: filename to open
+            mode: mode to open the file as
+        """
         return open(os.path.join(self.cwd, fn), mode)
 
+    def cd(self, d):
+        """
+        Change the current working directory.
 
+        Args:
+            d: directory to change to
+        """
+        self.cwd = os.path.join(self.cwd, d)
