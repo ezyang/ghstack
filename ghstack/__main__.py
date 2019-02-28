a0f34de7296e700fcb9529e9ccaf79533796eee5
Edward Z. Yang <ezyang@fb.com>
2019-02-28 10:47:44 -0500

    Go go types
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- ghstack/__main__.py
+++ ghstack/__main__.py
@@ -12,25 +12,30 @@ def main():
         help='Description of change you made')
     parser.add_argument(
         '--github-v4',
-        default=ghstack.endpoint.GraphQLEndpoint('https://api.github.com/graphql'),
+        default='https://api.github.com/graphql',
         help='GitHub GraphQL API endpoint (V4) to use')
     parser.add_argument(
         '--github-v3',
-        default=ghstack.endpoint.RESTEndpoint('https://api.github.com'),
+        default='https://api.github.com',
         help='GitHub REST API endpoint (V3) to use')
     args = parser.parse_args()
 
     conf = ghstack.config.read_config()
-
-    args.github_v4.oauth_token = conf.github_oauth
-    args.github_v3.oauth_token = conf.github_oauth
-    args.github_v4.proxy = conf.proxy
-    args.github_v3.proxy = conf.proxy
+    github_v4 = ghstack.endpoint.GraphQLEndpoint(
+        endpoint=args.github_v4,
+        oauth_token=conf.github_oauth,
+        proxy=conf.proxy
+    )
+    github_v3 = ghstack.endpoint.RESTEndpoint(
+        endpoint=args.github_v3,
+        oauth_token=conf.github_oauth,
+        proxy=conf.proxy
+    )
 
     ghstack.main(
         msg=args.msg,
-        github=args.github_v4,
-        github_rest=args.github_v3)
+        github=github_v4,
+        github_rest=github_v3)
 
 
 if __name__ == "__main__":
--- ghstack/endpoint.py
+++ ghstack/endpoint.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from typing import Optional, Dict, Any
 
 
 class GraphQLEndpoint(object):
@@ -8,21 +9,35 @@ class GraphQLEndpoint(object):
     At the moment, specifically engineered for GitHub.
     """
 
-    def __init__(self, endpoint, future=False):
+    # The URL of the endpoint to connect to
+    endpoint: str
+
+    # The string OAuth token to authenticate to the GraphQL server with
+    oauth_token: str
+
+    # The URL of a proxy to use for these connections (for
+    # Facebook users, this is typically http://fwdproxy:8080)
+    proxy: Optional[str]
+
+    # Whether or not this API lives "in the future".  Features in
+    # the future don't exist on the real GitHub API.
+    future: bool
+
+    def __init__(self,
+                 endpoint: str,
+                 oauth_token: str,
+                 proxy: Optional[str] = None,
+                 future: bool = False) -> None:
         """
         Args:
             endpoint: URL of the endpoint in question
         """
         self.endpoint = endpoint
-        # String OAuth token for authenticating to GitHub
-        self.oauth_token = None
-        # String proxy to use for http and https requests
-        self.proxy = None
-        # Whether or not this API lives "in the future".  Features in
-        # the future don't exist on the real GitHub API.
+        self.oauth_token = oauth_token
+        self.proxy = proxy
         self.future = future
 
-    def graphql(self, query, **kwargs):
+    def graphql(self, query: str, **kwargs: Any) -> Any:
         """
         Args:
             query: string GraphQL query to execute
@@ -31,6 +46,7 @@ class GraphQLEndpoint(object):
         headers = {}
         if self.oauth_token:
             headers['Authorization'] = 'bearer {}'.format(self.oauth_token)
+
         if self.proxy:
             proxies = {
                 'http': self.proxy,
@@ -38,12 +54,14 @@ class GraphQLEndpoint(object):
             }
         else:
             proxies = {}
+
         resp = requests.post(
             self.endpoint,
             json={"query": query, "variables": kwargs},
             headers=headers,
             proxies=proxies
         )
+
         # Actually, this code is dead on the GitHub GraphQL API, because
         # they seem to always return 200, even in error case (as of
         # 11/5/2018)
@@ -51,9 +69,12 @@ class GraphQLEndpoint(object):
             resp.raise_for_status()
         except requests.HTTPError:
             raise RuntimeError(json.dumps(resp.json(), indent=1))
+
         r = resp.json()
+
         if 'errors' in r:
             raise RuntimeError(json.dumps(r, indent=1))
+
         return r
 
 
@@ -63,40 +84,48 @@ class RESTEndpoint(object):
     At the moment, specifically engineered for GitHub.
     """
 
-    def __init__(self, endpoint):
+    # The base URL of the endpoint to connect to (all requests will
+    # be subpaths of this URL)
+    endpoint: str
+
+    # String OAuth token for authenticating to GitHub
+    oauth_token: str
+
+    # String proxy to use for http and https requests
+    proxy: Optional[str]
+
+    def __init__(self, endpoint: str, oauth_token: str, proxy: Optional[str] = None) -> None:
         self.endpoint = endpoint
-        # String OAuth token for authenticating to GitHub
-        self.oauth_token = None
-        # String proxy to use for http and https requests
-        self.proxy = None
+        self.oauth_token = oauth_token
+        self.proxy = proxy
 
-    def _headers(self):
+    def _headers(self) -> Dict[str, str]:
         return {
-          'Authorization': 'token ' + self.oauth_token,
-          'Content-Type': 'application/json',
-          'User-Agent': 'ghstack',
-          'Accept': 'application/vnd.github.v3+json',
-          }
+            'Authorization': 'token ' + self.oauth_token,
+            'Content-Type': 'application/json',
+            'User-Agent': 'ghstack',
+            'Accept': 'application/vnd.github.v3+json',
+        }
 
-    def get(self, path, **kwargs):
+    def get(self, path: str, **kwargs: Any) -> Any:
         """
         Send a GET request to endpoint 'path'.
         """
         return self.rest('get', path, **kwargs)
 
-    def post(self, path, **kwargs):
+    def post(self, path: str, **kwargs: Any) -> Any:
         """
         Send a POST request to endpoint 'path'.
         """
         return self.rest('post', path, **kwargs)
 
-    def patch(self, path, **kwargs):
+    def patch(self, path: str, **kwargs: Any) -> Any:
         """
         Send a PATCH request to endpoint 'path'.
         """
         return self.rest('patch', path, **kwargs)
 
-    def rest(self, method, path, **kwargs):
+    def rest(self, method: str, path: str, **kwargs: Any) -> Any:
         """
         Send a 'method' request to endpoint 'path'.
 
