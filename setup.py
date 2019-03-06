5a789b4524593bf12de5958aea94d18149116831
Edward Z. Yang <ezyang@fb.com>
2019-03-06 14:27:18 -0800

    Exclude graphql directory
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/ghexport",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("graphql", "graphql.*",)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
