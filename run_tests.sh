a10ed20f3ee91cd24000d4e7a9f8a90104fdaa66
Sam Estep <sestep@fb.com>
2021-06-09 21:44:05 -0400

    Use default filename for mypy config (#66)

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 set -ex
-# NB: must not apply detailed-mypy.ini to flake8, because
-# flake8 runs mypy on each file individually which means types
-# from imported things won't get processed correctly
 isort . --check --diff
 flake8
-mypy --config=detailed-mypy.ini .
+mypy .
 pytest --verbose
 echo "OK"
