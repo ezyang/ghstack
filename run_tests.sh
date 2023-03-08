42aaeac39a91942e9774c60623ebb3592940e3ad
Amethyst Reese <amy@n7.gg>
2023-03-08 15:33:54 -0800

    Improve virtualenv developer experience (#135)

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ex
-isort . --check --diff
-black --check .
-flake8
-mypy --install-types --non-interactive .
+isort --check --diff ghstack
+black --check ghstack
+flake8 ghstack
+mypy --install-types --non-interactive -m ghstack
 pytest --verbose
 echo "OK"
