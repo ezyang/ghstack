f55c93ad558b1e9ade1ff69c57c923a5ec700a4a
Edward Z. Yang <ezyang@meta.com>
2023-12-03 14:16:11 +0800

    Use lintrunner
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    [ghstack-poisoned]

--- run_tests.py
+++ run_tests.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 
-import subprocess
 import os
+import subprocess
 
 # Set environment variable
-os.environ['LIBCST_PARSER_TYPE'] = 'native'
+os.environ["LIBCST_PARSER_TYPE"] = "native"
 
 
 def main():
     # ufmt check ghstack
-    subprocess.run(['ufmt', 'check', 'ghstack'], check=True)
+    subprocess.run(["ufmt", "check", "ghstack"], check=True)
 
     # flake8 ghstack
-    subprocess.run(['flake8', 'ghstack'], check=True)
+    subprocess.run(["flake8", "ghstack"], check=True)
 
     # mypy --install-types --non-interactive -m ghstack
     subprocess.run(
-        ['mypy', '--install-types', '--non-interactive', '-m', 'ghstack'],
-        check=True)
+        ["mypy", "--install-types", "--non-interactive", "-m", "ghstack"], check=True
+    )
 
     # pytest --verbose
-    subprocess.run(['pytest', '--verbose'], check=True)
+    subprocess.run(["pytest", "--verbose"], check=True)
 
 
 if __name__ == "__main__":
