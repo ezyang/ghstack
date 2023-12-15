8bc12082403eb2d446380caa055cf07acd441ca6
Edward Z. Yang <ezyang@meta.com>
2023-12-15 09:10:44 +0800

    Update base for Update on "Cherry pick test online for direct"
    [ghstack-poisoned]

---
+++ ghstack/circleci_real.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+import re
+from typing import Any, Optional
+
+import aiohttp
+
+import ghstack.cache
+import ghstack.circleci
+
+RE_BUILD_PATH = re.compile(r"^project/github/[^/]+/[^/]+/[0-9]+$")
+
+
+class RealCircleCIEndpoint(ghstack.circleci.CircleCIEndpoint):
+    rest_endpoint: str = "https://circleci.com/api/v1.1"
+
+    # The API token to authenticate to CircleCI with
+    # https://circleci.com/account/api
+    circle_token: Optional[str]
+
+    # The URL of a proxy to use for these connections (for
+    # Facebook users, this is typically 'http://fwdproxy:8080')
+    proxy: Optional[str]
+
+    def __init__(
+        self, *, circle_token: Optional[str] = None, proxy: Optional[str] = None
+    ):
+        self.circle_token = circle_token
+        self.proxy = proxy
+
+    async def rest(self, method: str, path: str, **kwargs: Any) -> Any:
+        headers = {
+            "Content-Type": "application/json",
+            "User-Agent": "ghstack",
+        }
+
+        url = self.rest_endpoint + "/" + path
+        logging.debug("# {} {}".format(method, url))
+        logging.debug("Request body:\n{}".format(json.dumps(kwargs, indent=1)))
+
+        params = {}
+        if self.circle_token:
+            params["circle-token"] = self.circle_token
+
+        is_get_build = method == "get" and RE_BUILD_PATH.match(path)
+
+        if is_get_build:
+            # consult cache
+            cache_result = ghstack.cache.get("circleci", path)
+            if cache_result is not None:
+                logging.debug("Retrieved result from cache")
+                return json.loads(cache_result)
+
+        async with aiohttp.request(
+            method.upper(),
+            url,
+            params=params,
+            json=kwargs,
+            headers=headers,
+            proxy=self.proxy,
+        ) as resp:
+            logging.debug("Response status: {}".format(resp.status))
+
+            r_text = await resp.text()
+
+            try:
+                r = json.loads(r_text)
+            except json.decoder.JSONDecodeError:
+                logging.debug("Response body:\n{}".format(r_text))
+                raise
+            else:
+                pretty_json = json.dumps(r, indent=1)
+                logging.debug("Response JSON:\n{}".format(pretty_json))
+
+            try:
+                resp.raise_for_status()
+            except aiohttp.ClientResponseError:
+                raise RuntimeError(pretty_json)
+
+            # NB: Don't save to cache if it's still running
+            if is_get_build and r["outcome"] is not None:
+                logging.debug("Saving result to cache")
+                ghstack.cache.put("circleci", path, r_text)
+
+            return r
---
+++ ghstack/cli.py
@@ -0,0 +1,284 @@
+import asyncio
+import contextlib
+from typing import Generator, List, Optional, Tuple
+
+import click
+
+import ghstack
+import ghstack.action
+import ghstack.checkout
+import ghstack.circleci_real
+import ghstack.config
+import ghstack.github_real
+import ghstack.land
+import ghstack.logs
+import ghstack.rage
+import ghstack.status
+import ghstack.submit
+import ghstack.unlink
+
+EXIT_STACK = contextlib.ExitStack()
+
+GhstackContext = Tuple[
+    ghstack.shell.Shell,
+    ghstack.config.Config,
+    ghstack.github_real.RealGitHubEndpoint,
+]
+
+
+@contextlib.contextmanager
+def cli_context(
+    *,
+    request_circle_token: bool = False,
+    request_github_token: bool = True,
+) -> Generator[GhstackContext, None, None]:
+    with EXIT_STACK:
+        shell = ghstack.shell.Shell()
+        config = ghstack.config.read_config(
+            request_circle_token=request_circle_token,
+            request_github_token=request_github_token,
+        )
+        github = ghstack.github_real.RealGitHubEndpoint(
+            oauth_token=config.github_oauth,
+            proxy=config.proxy,
+            github_url=config.github_url,
+        )
+        yield shell, config, github
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+@click.version_option(ghstack.__version__, "--version", "-V")
+@click.option("--debug", is_flag=True, help="Log debug information to stderr")
+# hidden arguments that we'll pass along to submit if no other command given
+@click.option("--message", "-m", default="Update", hidden=True)
+@click.option("--update-fields", "-u", is_flag=True, hidden=True)
+@click.option("--short", is_flag=True, hidden=True)
+@click.option("--force", is_flag=True, hidden=True)
+@click.option("--no-skip", is_flag=True, hidden=True)
+@click.option("--draft", is_flag=True, hidden=True)
+@click.option("--direct", is_flag=True, hidden=True)
+@click.option("--base", "-B", default=None, hidden=True)
+@click.option("--stack/--no-stack", "-s/-S", is_flag=True, default=True, hidden=True)
+def main(
+    ctx: click.Context,
+    debug: bool,
+    message: str,
+    update_fields: bool,
+    short: bool,
+    force: bool,
+    direct: bool,
+    no_skip: bool,
+    draft: bool,
+    base: Optional[str],
+    stack: bool,
+) -> None:
+    """
+    Submit stacks of diffs to Github
+    """
+    EXIT_STACK.enter_context(ghstack.logs.manager(debug=debug))
+
+    if not ctx.invoked_subcommand:
+        ctx.invoke(
+            submit,
+            message=message,
+            update_fields=update_fields,
+            short=short,
+            force=force,
+            no_skip=no_skip,
+            draft=draft,
+            base=base,
+            stack=stack,
+            direct=direct,
+        )
+
+
+@main.command("action")
+@click.option("--close", is_flag=True, help="Close the specified pull request")
+@click.argument("pull_request", metavar="PR")
+def action(close: bool, pull_request: str) -> None:
+    """
+    Perform actions on a PR
+    """
+    with cli_context() as (shell, _, github):
+        ghstack.action.main(
+            pull_request=pull_request,
+            github=github,
+            sh=shell,
+            close=close,
+        )
+
+
+@main.command("checkout")
+@click.argument("pull_request", metavar="PR")
+def checkout(pull_request: str) -> None:
+    """
+    Checkout a PR
+    """
+    with cli_context(request_github_token=False) as (shell, config, github):
+        ghstack.checkout.main(
+            pull_request=pull_request,
+            github=github,
+            sh=shell,
+            remote_name=config.remote_name,
+        )
+
+
+@main.command("land")
+@click.option("--force", is_flag=True, help="force land even if the PR is closed")
+@click.argument("pull_request", metavar="PR")
+def land(force: bool, pull_request: str) -> None:
+    """
+    Land a PR stack
+    """
+    with cli_context() as (shell, config, github):
+        ghstack.land.main(
+            pull_request=pull_request,
+            github=github,
+            sh=shell,
+            github_url=config.github_url,
+            remote_name=config.remote_name,
+            force=force,
+        )
+
+
+@main.command("rage")
+@click.option(
+    "--latest",
+    is_flag=True,
+    help="Select the last command (not including rage commands) to report",
+)
+def rage(latest: bool) -> None:
+    with cli_context(request_github_token=False):
+        ghstack.rage.main(latest)
+
+
+@main.command("status")
+@click.argument("pull_request", metavar="PR")
+def status(pull_request: str) -> None:
+    """
+    Check status of a PR
+    """
+    with cli_context(request_circle_token=True) as (shell, config, github):
+        circleci = ghstack.circleci_real.RealCircleCIEndpoint(
+            circle_token=config.circle_token
+        )
+
+        fut = ghstack.status.main(
+            pull_request=pull_request,
+            github=github,
+            circleci=circleci,
+        )
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(fut)
+        loop.close()
+
+
+@main.command("submit")
+@click.option(
+    "--message",
+    "-m",
+    default="Update",
+    help="Description of change you made",
+)
+@click.option(
+    "--update-fields",
+    "-u",
+    is_flag=True,
+    help="Update GitHub pull request summary from the local commit",
+)
+@click.option(
+    "--short", is_flag=True, help="Print only the URL of the latest opened PR to stdout"
+)
+@click.option(
+    "--force",
+    is_flag=True,
+    help="force push the branch even if your local branch is stale",
+)
+@click.option(
+    "--no-skip",
+    is_flag=True,
+    help="Never skip pushing commits, even if the contents didn't change "
+    "(use this if you've only updated the commit message).",
+)
+@click.option(
+    "--draft",
+    is_flag=True,
+    help="Create the pull request in draft mode (only if it has not already been created)",
+)
+@click.option(
+    "--base",
+    "-B",
+    default=None,
+    help="Branch to base the stack off of; "
+    "defaults to the default branch of a repository",
+)
+@click.option(
+    "--stack/--no-stack",
+    "-s/-S",
+    is_flag=True,
+    default=True,
+    help="Submit the entire of stack of commits reachable from HEAD, versus only single commits.  "
+    "This affects the meaning of REVS.  With --stack, we submit all commits that "
+    "are reachable from REVS, excluding commits already on the base branch.  Revision ranges "
+    "supported by git rev-list are also supported.  "
+    "With --no-stack, we support only non-range identifiers, and will submit each commit "
+    "listed in the command line.",
+)
+@click.option(
+    "--direct", is_flag=True, help="Create stack that directly merges into master"
+)
+@click.argument(
+    "revs",
+    nargs=-1,
+    metavar="REVS",
+)
+def submit(
+    message: str,
+    update_fields: bool,
+    short: bool,
+    force: bool,
+    no_skip: bool,
+    draft: bool,
+    direct: bool,
+    base: Optional[str],
+    revs: Tuple[str, ...],
+    stack: bool,
+) -> None:
+    """
+    Submit or update a PR stack
+    """
+    with cli_context() as (shell, config, github):
+        ghstack.submit.main(
+            msg=message,
+            username=config.github_username,
+            sh=shell,
+            github=github,
+            update_fields=update_fields,
+            short=short,
+            force=force,
+            no_skip=no_skip,
+            draft=draft,
+            github_url=config.github_url,
+            remote_name=config.remote_name,
+            base_opt=base,
+            revs=revs,
+            stack=stack,
+            direct=direct,
+        )
+
+
+@main.command("unlink")
+@click.argument("commits", nargs=-1, metavar="COMMIT")
+def unlink(commits: List[str]) -> None:
+    """
+    Unlink commits from PRs
+    """
+    with cli_context() as (shell, config, github):
+        ghstack.unlink.main(
+            commits=commits,
+            github=github,
+            sh=shell,
+            github_url=config.github_url,
+            remote_name=config.remote_name,
+        )
