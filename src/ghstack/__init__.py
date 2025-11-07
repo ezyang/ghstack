fc091fbbe54fb00e8ade6a2e375201470be59034
Nikita Shulga <nshulga@meta.com>
2025-11-07 10:57:26 -0800

    Move to main

--- src/ghstack/__init__.py
+++ src/ghstack/__init__.py
@@ -7,10 +7,4 @@ if sys.version_info >= (3, 8):
 else:
     import importlib_metadata
 
-if sys.version_info >= (3, 14):
-    # Create new event loop as asyncio.get_event_loop() throws runtime error in 3.14
-    import asyncio as _asyncio
-
-    _asyncio.set_event_loop(_asyncio.new_event_loop())
-
 __version__ = importlib_metadata.version("ghstack")  # type: ignore[no-untyped-call]
