9d5ca134932b8660dca6f2f8ce66a2d256a04ec9
Edward Yang <ezyang@meta.com>
2025-12-16 09:40:17 -0500

    Add --same-base flag to ensure checkout is safe.
    Signed-off-by: Edward Yang <ezyang@meta.com>
    ghstack-source-id: 370f9655353759c7887e32f426218b86ff7707c5
    ghstack-comment-id: 3660894244
    Pull-Request: https://github.com/ezyang/ghstack/pull/306

--- src/ghstack/checkout.py
+++ src/ghstack/checkout.py
@@ -13,6 +13,7 @@ def main(
     github: ghstack.github.GitHubEndpoint,
     sh: ghstack.shell.Shell,
     remote_name: str,
+    same_base: bool = False,
 ) -> None:
 
     params = ghstack.github_utils.parse_pull_request(
@@ -27,5 +28,37 @@ def main(
 
     # TODO: Handle remotes correctly too (so this subsumes hub)
 
+    # If --same-base is specified, check if checkout would change the merge-base
+    if same_base:
+        # Get the default branch name from the repo
+        repo_info = ghstack.github_utils.get_github_repo_info(
+            github=github,
+            sh=sh,
+            repo_owner=params["owner"],
+            repo_name=params["name"],
+            github_url=params["github_url"],
+            remote_name=remote_name,
+        )
+        default_branch = repo_info["default_branch"]
+        default_branch_ref = f"{remote_name}/{default_branch}"
+
+        # Get current merge-base with default branch
+        current_base = sh.git("merge-base", default_branch_ref, "HEAD")
+    else:
+        current_base = None
+        default_branch_ref = None
+
     sh.git("fetch", "--prune", remote_name)
+
+    # If --same-base is specified, check what the new merge-base would be
+    if same_base:
+        target_ref = remote_name + "/" + orig_ref
+        new_base = sh.git("merge-base", default_branch_ref, target_ref)
+
+        if current_base != new_base:
+            raise RuntimeError(
+                f"Checkout would change merge-base from {current_base[:8]} to {new_base[:8]}, "
+                f"aborting due to --same-base flag"
+            )
+
     sh.git("checkout", remote_name + "/" + orig_ref)
